71daa3ce047aa6595b5cdac0a007ac2ea68959c6
kristofferbv <39952448+kristofferbv@users.noreply.github.com>
2023-06-10 01:47:47 +0200

    some updates to choose high demand data
    Some code might need to be modified. For now, it works on the 2,2,2,2 distribution of categories with seed = 2

--- MIP/main.py
+++ MIP/main.py
@@ -39,10 +39,11 @@ if __name__ == '__main__':
         product_categories = config["deterministic_model"]["product_categories"]
         seed = config["main"]["seed"]
         n_products = sum(product_categories.values())
-        
+        # retrieve_data.categorize_products("sales_orders.csv", "w", True)
+
 
         # calculate average unit costs to compute setup costs
-        all_products = retrieve_data.read_products("2016-01-01", "2020-12-30")
+        all_products = retrieve_data.read_products("2017-01-01", "2020-12-30")
 
         unit_price_all = [df.iloc[0]['average_unit_price'] for df in all_products]
 
@@ -55,19 +56,35 @@ if __name__ == '__main__':
         if seed is not None:
             # Setting a random seed ensure we select the same random products each time
             random.seed(seed)
-            
+
         for category in product_categories.keys():
-            category_products = retrieve_data.read_products("2016-01-01", "2020-12-30", category)
-            print(category)
+            category_products = retrieve_data.read_products("2017-01-01", "2020-12-30", category)
+
+            # The following code is to find products with high demand. (Might need to be removed
+            # for other instances than 2-2-2-2)
+            if category == "erratic":
+                # Filter products based on mean sales quantity
+                category_products = [product for product in category_products if product["sales_quantity"].mean() > 20 and product["sales_quantity"].max() < 100]
+                print("category", category_products)
+            elif category == "smooth":
+                category_products = [product for product in category_products if product["sales_quantity"].mean() > 3 and product["sales_quantity"].max() < 1000]
+            elif category == "intermittent":
+                category_products = [product for product in category_products if (product["sales_quantity"] > 0).any() and (product.loc[product["sales_quantity"] > 0, "sales_quantity"].mean() > 2)]
+            else:
+                category_products = [product for product in category_products if (product["sales_quantity"] > 0).any() and (product.loc[product["sales_quantity"] > 0, "sales_quantity"].mean() > 3)]
+
             number_of_products = product_categories[category]
-            print(number_of_products)
-            if number_of_products > 0:
-                products += random.sample(category_products, product_categories[category])
+
+            if number_of_products > 0 and len(category_products) > 0:
+                # Make sure the number of products required does not exceed the number of available products
+                number_of_products = min(number_of_products, len(category_products))
+
+                products += random.sample(category_products, number_of_products)
 
         # products = retrieve_data.read_products_with_hashes("2016-01-10", "2020-12-30", ["569b6782ce5885fc4abf21cfde38f7d7", "92b1f191dfce9fff64b4effd954ccaab", "8ef91aac79542f11dedec4f79265ae3a", "2fa9c91f40d6780fd5b3c219699eb139", "1fb096daa569c811723ce8796722680e", "f7b3622f9eb50cb4eee149127c817c79"])
         # products = [df["sales_quantity"] for df in products]
+        plot_sales_quantity(products)
 
-        start_date = products[0].index[208]
 
         # products = generate_seasonal_data_based_on_products(products, 500, seed = 1)
         # sarima.forecast(products[0], start_date, 20)
@@ -84,5 +101,6 @@ if __name__ == '__main__':
         if generate_new_data:
             simulation.simulate(products, config)
         else:
+            start_date = products[0].index[104]
             simulation_length = config["simulation"]["simulation_length"]
             simulation.run_one_episode(start_date, n_time_periods, simulation_length, products, config)
