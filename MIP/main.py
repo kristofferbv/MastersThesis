3dad28b2d0e87af958778166b9ea8acc888952c8
kristofferbv <39952448+kristofferbv@users.noreply.github.com>
2023-06-10 20:35:27 +0200

    updating to choose the categories with highest demand

--- MIP/main.py
+++ MIP/main.py
@@ -2,7 +2,6 @@ import random
 import os
 import sys
 
-
 # Get the path of the current script
 current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -22,11 +21,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # Change the working directory to the directory of the current script
 os.chdir(current_dir)
 
-
-
 if __name__ == '__main__':
 
-    #config_files = ["config.yml", "config2.yml", "config3.yml", "config4.yml", "config5.yml", "config6.yml", "config7.yml","config8.yml","config9.yml","config10.yml"]
+    # config_files = ["config.yml", "config2.yml", "config3.yml", "config4.yml", "config5.yml", "config6.yml", "config7.yml","config8.yml","config9.yml","config10.yml"]
 
     config_files = ["config.yml"]
 
@@ -34,13 +31,12 @@ if __name__ == '__main__':
         config = load_config(config_file)
         n_time_periods = config["deterministic_model"]["n_time_periods"]  # number of time periods
         should_analyse = config["main"]["should_analyse"]
-        use_stationary_data = False # config["main"]["stationary_products"]
+        use_stationary_data = False  # config["main"]["stationary_products"]
         generate_new_data = config["main"]["generate_new_data"]
         product_categories = config["deterministic_model"]["product_categories"]
         seed = config["main"]["seed"]
         n_products = sum(product_categories.values())
         # retrieve_data.categorize_products("sales_orders.csv", "w", True)
-
 
         # calculate average unit costs to compute setup costs
         all_products = retrieve_data.read_products("2017-01-01", "2020-12-30")
@@ -59,19 +55,8 @@ if __name__ == '__main__':
 
         for category in product_categories.keys():
             category_products = retrieve_data.read_products("2017-01-01", "2020-12-30", category)
-
-            # The following code is to find products with high demand. (Might need to be removed
-            # for other instances than 2-2-2-2)
-            if category == "erratic":
-                # Filter products based on mean sales quantity
-                category_products = [product for product in category_products if product["sales_quantity"].mean() > 20 and product["sales_quantity"].max() < 100]
-                print("category", category_products)
-            elif category == "smooth":
-                category_products = [product for product in category_products if product["sales_quantity"].mean() > 3 and product["sales_quantity"].max() < 1000]
-            elif category == "intermittent":
-                category_products = [product for product in category_products if (product["sales_quantity"] > 0).any() and (product.loc[product["sales_quantity"] > 0, "sales_quantity"].mean() > 2)]
-            else:
-                category_products = [product for product in category_products if (product["sales_quantity"] > 0).any() and (product.loc[product["sales_quantity"] > 0, "sales_quantity"].mean() > 3)]
+            category_products = [product for product in category_products if product["sales_quantity"].mean() <= 150]
+            category_products.sort(key=lambda product: product["sales_quantity"].mean())
 
             number_of_products = product_categories[category]
 
@@ -80,27 +65,25 @@ if __name__ == '__main__':
                 number_of_products = min(number_of_products, len(category_products))
 
                 products += random.sample(category_products, number_of_products)
+            print("len", len(products))
 
         # products = retrieve_data.read_products_with_hashes("2016-01-10", "2020-12-30", ["569b6782ce5885fc4abf21cfde38f7d7", "92b1f191dfce9fff64b4effd954ccaab", "8ef91aac79542f11dedec4f79265ae3a", "2fa9c91f40d6780fd5b3c219699eb139", "1fb096daa569c811723ce8796722680e", "f7b3622f9eb50cb4eee149127c817c79"])
         # products = [df["sales_quantity"] for df in products]
         plot_sales_quantity(products)
 
-
-        # products = generate_seasonal_data_based_on_products(products, 500, seed = 1)
-        # sarima.forecast(products[0], start_date, 20)
-        # holt_winters_method.forecast(products[0], start_date, True, 20)
-
-        if should_analyse:  # analysing plotting, decomposing and testing for stationarity
-            plot_sales_quantity(products)
-            get_non_stationary_products(products, should_plot=True, verbose=True)
-            for i, product in enumerate(products):
-                if isinstance(product, pd.DataFrame):
-                    decompose_sales_quantity(product, product["product_hash"].iloc[0])
-                else:
-                    decompose_sales_quantity(product, str(i))
-        if generate_new_data:
-            simulation.simulate(products, config)
-        else:
-            start_date = products[0].index[104]
-            simulation_length = config["simulation"]["simulation_length"]
-            simulation.run_one_episode(start_date, n_time_periods, simulation_length, products, config)
+        for i in np.arange(1, 0, -0.1):
+            beta = i
+            if should_analyse:  # analysing plotting, decomposing and testing for stationarity
+                plot_sales_quantity(products)
+                get_non_stationary_products(products, should_plot=True, verbose=True)
+                for i, product in enumerate(products):
+                    if isinstance(product, pd.DataFrame):
+                        decompose_sales_quantity(product, product["product_hash"].iloc[0])
+                    else:
+                        decompose_sales_quantity(product, str(i))
+            if generate_new_data:
+                simulation.simulate(products, config, beta=beta)
+            else:
+                start_date = products[0].index[104]
+                simulation_length = config["simulation"]["simulation_length"]
+                simulation.run_one_episode(start_date, n_time_periods, simulation_length, products, config)
